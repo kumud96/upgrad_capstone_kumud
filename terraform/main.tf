68c6637c0b6540cf6a58f456187f08e647989f2f
kumud96 <kumud.sharma0408@gmail.com>
2022-07-02 19:35:17 +0530

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "upgrad-kumud-capstone"
+    key    = "path/state"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.63.0"
+    }
+ }
+}
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIASRPUN7KG33CCXYPI"
+  secret_key = "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"
+}
